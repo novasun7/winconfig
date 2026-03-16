d7a7cf4014e16b996b173eb4521b5ff045c2b937
Paul Smith <paul@psmith.me>
2026-03-16 08:34:09 -0600

    Remove alias of doskey, didn't work right. Add some aliases.

--- aliases.bat
+++ aliases.bat
@@ -1,21 +1,28 @@
 @echo off
 
-doskey alias=if ".$*." == ".." (doskey /macros) else (doskey $*)
-rem should have unalias command
-doskey unalias=doskey $*=
-
-doskey vi=gvim $*
-rem doskey edit="C:\Program Files (x86)\Notepad++\notepad++.exe" $*
-doskey l=dir/ogn $*
-
 rem Turn off prompting for date/time
 doskey time=time/t 
 doskey date=date/t
 
-rem Default editor - should be in path
-doskey edit=notepad $*
-
+rem aliases for bash commands
+doskey l=dir/ogn $*
 doskey pwd=cd
 doskey home=cd %USERPROFILE%
-
+doskey diff=comp $*
+doskey cat=type $*
+doskey ls=dir/w $*
+doskey ll=dir $*
+doskey clear=cls
+doskey cp=copy $*
 doskey mv=move $*
+doskey vi=gvim $*
+doskey rm=del $*
+
+rem aliases for directories
+doskey od=cd "%OneDrive%"
+doskey dev=cd "%USERPROFILE%\dev"
+doskey bin=cd "%USERPROFILE%\bin"
+
+rem py alias for build scripts
+doskey py=python $*
+
